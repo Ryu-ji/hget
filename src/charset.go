7056f59ae30645fef103986cac6394f2400dc8ec
Ryuji <gufbguf.gm+hosting@gmail.com>
2020-06-26 03:53:46 +0900

    Create charset.go

---
+++ src/charset.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+func bytesToBase64(bytes []byte) string {
+	return base64.StdEncoding.EncodeToString(bytes)
+}
+
+func bytestoBase32(bytes []byte) string {
+	return base32.StdEncoding.EncodeToString(bytes)
+}
+*/
+/*実装予定
+func changeCharset(str string) {
+
+}
+*/
